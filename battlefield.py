6e665358c6d265b321713b6f759bbb906f0d1c02
Lucy Ruiz <lucyruiz846@gmail.com>
2022-08-28 13:15:15 -0700

    breaking change: Project completed. Refined prints to diplay a better message in each battle and completed display_winner method.

--- battlefield.py
+++ battlefield.py
@@ -4,7 +4,7 @@ from dinosaur import Dinosaur
 class Battlefield:
 
     def __init__(self):
-        self.dinosaur = Dinosaur("Pierre", 18)
+        self.dinosaur = Dinosaur("Pierre", 20)
         self.robot = Robot("Francis")
 
     def run_game(self):
@@ -16,11 +16,19 @@ class Battlefield:
         print(f'Welcome to the battle of Dinosaur {self.dinosaur.name} vs Robot {self.robot.name}')
 
     def battle_phase(self):
-        while self.dinosaur.health > 0 or self.robot.health > 0:
+        while self.dinosaur.health > 0 and self.robot.health > 0:
             self.dinosaur.attack(self.robot)
-            print(f'Dinosaur attacked Robot')
-            self.robot.attack(self.dinosaur)
-            print(f'Robot attacked Dinosaur')
+            print(f'''Dinosaur attacked Robot
+            Robot health: {self.robot.health}
+            Dinosaur health: {self.dinosaur.health}''')
+            if self.robot.health > 0:
+                self.robot.attack(self.dinosaur)
+                print(f'''Robot attacked Dinosaur
+                Dinosaur health: {self.dinosaur.health}
+                Robot health: {self.robot.health}''')
 
     def display_winner(self):
-        pass
+        if self.robot.health > 0:
+            print('Robot wins')
+        else:
+            print('Dinosaur wins')
